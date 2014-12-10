3557aee53922319694ef0003cb0d05c784b779f3
cheyang <cheyang@cn.ibm.com>
2014-12-10 16:53:26 +0800

    add banner

--- lib/brick/cli/model_create.rb
+++ lib/brick/cli/model_create.rb
@@ -4,9 +4,9 @@ class Brick
   class CLI
     class ModelCreate < CLI
       
-      puts "logger class=#{logger}"
       
-      logger.info 'brick model create MODEL(options)'
+      
+      banner 'brick model create MODEL(options)'
       
     end
   end
