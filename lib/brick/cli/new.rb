3aec553e91a83eafa976c25ba539cdc83bc14e2c
cheyang <cheyang@cn.ibm.com>
2014-12-10 20:30:35 +0800

    add logger

--- lib/brick/cli/new.rb
+++ lib/brick/cli/new.rb
@@ -4,7 +4,7 @@ class Brick
       banner 'brick new MODEL(options)'
       
       def run
-        logger.info "hello, #{banner}"
+        CLI::logger.info "hello, #{banner}"
       end
       
     end
