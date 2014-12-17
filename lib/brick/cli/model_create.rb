a0c4bdb98b15f4dfa644ab034e2a413ce687d73b
cheyang <cheyang@cn.ibm.com>
2014-12-17 14:50:24 +0800

    add model create for test

--- lib/brick/cli/model_create.rb
+++ lib/brick/cli/model_create.rb
@@ -8,6 +8,10 @@ module Brick
       #it's from Mixlib::CLI
       banner 'brick model create MODEL(options)'
       
+      def run
+        CLI::logger.info "hello, #{banner}"
+      end
+      
     end
   end
 end
