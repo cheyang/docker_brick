475bf21a1679b5217fab1b9292f575a1ac8fbe7d
cheyang <cheyang@cn.ibm.com>
2014-12-19 15:45:02 +0800

    add rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'rspec'
+require 'rspec/its'
+
+
+
+RSpec.shared_context "local paths" do
+  def project_dir
+    File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  end
+end
+
