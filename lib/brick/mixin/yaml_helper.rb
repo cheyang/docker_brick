647f84a69c20e455330da98f61fd18d1e60031b3
cheyang <cheyang@cn.ibm.com>
2014-12-20 20:53:36 +0800

    use yaml to replace psych

--- lib/brick/mixin/yaml_helper.rb
+++ lib/brick/mixin/yaml_helper.rb
@@ -1,8 +1,10 @@
+require 'yaml'
+
 module Brick::Mixin
   module YamlHelper
     def load_yaml_file(path, expected_type = Hash)
       raise("Cannot find file `#{path}'") unless File.exists?(path)
-      yaml = ::Psych.load_file(path)
+      yaml = YAML::load_file(path)
       
       if expected_type && !yaml.is_a?(expected_type)
         raise "Incorrect file format in `#{path}', #{expected_type} expected"
