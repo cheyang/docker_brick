f9d87f0d1f3ec73467ab1a4844fdc9e79a91184a
cheyang <cheyang@cn.ibm.com>
2014-12-20 22:31:32 +0800

    check image

--- spec/unit/brick/models/project_spec.rb
+++ spec/unit/brick/models/project_spec.rb
@@ -11,7 +11,7 @@ describe Brick::Models::Project do
    
    describe '#initialize' do
      it "parse services correctly" do
-       subject.services.each{|x| puts x.name; puts x.service_config.images}
+       subject.services.each{|x| puts x.name; puts x.service_config.image}
      end
    end
 end
