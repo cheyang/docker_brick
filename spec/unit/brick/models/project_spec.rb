ae2f10b0207cbf01ef0ac4010a570f8b589bb48c
cheyang <cheyang@cn.ibm.com>
2014-12-20 20:02:47 +0800

    add test

--- spec/unit/brick/models/project_spec.rb
+++ spec/unit/brick/models/project_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 
 describe Brick::Models::Project do
-   subject { described_class.new({:config_file=>'fig_complete.yml'}) }
+  
+   before :all do
+     @config_file = File.join(File.dirname(__FILE__),'fig_complete.yml' )
+   end
+   
+   subject { described_class.new({:config_file=>@config_file}) }
    
    describe '#initialize' do
      it "parse services correctly" do
