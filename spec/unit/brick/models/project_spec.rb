fa9d31f8a210b66831c08c4d30e3296d55cc2c48
cheyang <cheyang@cn.ibm.com>
2014-12-21 17:57:48 +0800

    config hash

--- spec/unit/brick/models/project_spec.rb
+++ spec/unit/brick/models/project_spec.rb
@@ -18,7 +18,7 @@ describe Brick::Models::Project do
        
        nginx.images = "nginx:latest"
        
-        subject.services.each{|service|puts "links=#{service.service_config.links}"; puts service.service_config.inspect}
+        subject.services.each{|service|puts "links=#{service.service_config.links}"; puts service.service_config.to_h}
        
        subject.services[0].name.should eq 'nginx'
        
