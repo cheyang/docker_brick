0c51fd70f8b46a176a102fdc2bd2a4e4d5cacb20
cheyang <cheyang@163.com>
2014-12-22 16:14:38 +0800

    add mixin

--- lib/brick.rb
+++ lib/brick.rb
@@ -2,9 +2,7 @@ require "brick/version"
 
 require "brick/cli/core/subcommand_loader"
 
-require "brick/mixin/convert_to_class_name"
 
-require "brick/mixin/aliasing"
 
 require "brick/cli__validator"
 
@@ -13,3 +11,6 @@ require 'brick/models/models'
 require "logger"
 
 require 'brick/monkey_patches/hash'
+
+
+require 'brick/mixin'
---
+++ lib/brick/mixin/mixin.rb
@@ -0,0 +1,11 @@
+module Brick
+  module Mixin
+    
+  end
+end
+
+require "brick/mixin/convert_to_class_name"
+
+require "brick/mixin/aliasing"
+
+require 'brick/mixin/docker_support'
