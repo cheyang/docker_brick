9f267e0d26b5964fad21e9c0b602c6b0ac25c5ed
cheyang <cheyang@163.com>
2015-01-02 12:58:53 +0800

    optparser

--- lib/brick/cli/help.rb
+++ lib/brick/cli/help.rb
@@ -7,7 +7,7 @@ class Brick::CLI
       banner 'brick help'
       
       def run
-        Brick::CLI.common_optparser.banner=self.banner
+        ::Brick::CLI.common_optparser.opt_parser.banner=self.banner
         ::Brick::CLI_Validator::print_help_and_exit(0)
       end
       
