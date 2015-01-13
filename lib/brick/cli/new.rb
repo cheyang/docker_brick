94797523d1c603511444f739519237ee27349f9f
cheyang <cheyang@163.com>
2015-01-13 10:50:34 +0800

    new project feature

--- lib/brick/cli/new.rb
+++ lib/brick/cli/new.rb
@@ -2,13 +2,24 @@ require 'brick/cli'
 
 class Brick::CLI
   
-    class New < Brick::CLI
-      banner 'brick new PROJECT(options)'
-      
-      def run
-        CLI::logger.info "hello, #{banner}"
+  class New < Brick::CLI
+    banner 'brick new PROJECT(options)'
+    
+    def run
+     
+      if @cmd_args.size > 0
+        project_name = @cmd_args[0]
+      else
+        puts "You must specify the project name to create"
+        puts banner
+        exit 1
       end
       
+      project_dir=::Brick::Config[:project_dir]
+      
+      Brick::Generators::NewProjectGenerator.start([project_dir])
     end
+    
+  end
   
 end
--- lib/brick/generators/new_project_generator.rb
+++ lib/brick/generators/new_project_generator.rb
@@ -3,13 +3,15 @@ require "fileutils"
 
 module Brick::Generators
   class NewProjectGenerator < Thor::Group
+    
+     class_options :project_directory
      
      def self.source_root
         File.join(File.dirname(__FILE__), "new_project_generator", "templates")
      end
     
      def create_root
-        self.destination_root = File.expand_path(::Brick::Config[:project_dir])
+        self.destination_root = File.expand_path(option[:project_directory])
         empty_directory '.'
         FileUtils.cd(destination_root)
     end
