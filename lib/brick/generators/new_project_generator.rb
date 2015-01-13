439c23f84038a24aa42ea2003b2cdef4df87289a
cheyang <cheyang@163.com>
2015-01-13 12:25:22 +0800

    use accessor

--- lib/brick/generators/new_project_generator.rb
+++ lib/brick/generators/new_project_generator.rb
@@ -8,6 +8,8 @@ module Brick::Generators
      argument :working_dir
      argument :project_name
      
+     attr_accessor :project_root
+     
      def self.source_root
         File.join(File.dirname(__FILE__), "new_project_generator", "templates")
      end
@@ -15,18 +17,18 @@ module Brick::Generators
      def create_root
         self.destination_root = File.expand_path(working_dir)
         empty_directory(project_name)
-        @project_root=File.join(working_dir,project_name)
-        FileUtils.cd(@project_root)
+        self.project_root=File.join(working_dir,project_name)
+        FileUtils.cd(project_root)
     end
     
     def fig_file
       copy_file("fig.yml","#{project_name}/fig.yml")
-      puts "the project is created at #{@project_root}"
+      puts "the project is created at #{project_root}"
     end
     
     def init_git
-      puts "Initializing git repo in #{@project_root}"
-      FileUtils.cd(@project_root)
+      puts "Initializing git repo in #{project_root}"
+      FileUtils.cd(project_root)
       git :init
       git :add => "."
       git :commit => "-m 'Initial commit'"
