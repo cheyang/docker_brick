46239512ce9b5aaf2eb17fcc45fb82818a219cca
cheyang <cheyang@163.com>
2015-01-13 11:05:00 +0800

    change to argument

--- lib/brick/generators/new_project_generator.rb
+++ lib/brick/generators/new_project_generator.rb
@@ -4,14 +4,14 @@ require "fileutils"
 module Brick::Generators
   class NewProjectGenerator < Thor::Group
     
-     class_options :project_directory
+     argument :project_directory
      
      def self.source_root
         File.join(File.dirname(__FILE__), "new_project_generator", "templates")
      end
     
      def create_root
-        self.destination_root = File.expand_path(option[:project_directory])
+        self.destination_root = File.expand_path(project_directory)
         empty_directory '.'
         FileUtils.cd(destination_root)
     end
