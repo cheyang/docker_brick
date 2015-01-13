9c18763321f0d9c848773c9fe13c95f68b9be04c
cheyang <cheyang@163.com>
2015-01-13 16:13:00 +0800

    avoid the hash to be changed

--- lib/brick/docker/docker_client.rb
+++ lib/brick/docker/docker_client.rb
@@ -70,13 +70,13 @@ module Brick
        docker_hash['name'] = name unless name.nil?
         
         begin
-          container = ::Docker::Container.create(docker_hash, connection)
+          container = ::Docker::Container.create(docker_hash.dup, connection)
           #get full information of the container
           container = ::Docker::Container.get(container.id,connection)
         rescue ::Docker::Error::NotFoundError => exception
             if exception.message.include? 'No such image'
               ::Docker::Image.create({'fromImage'=> config_hash['image']},{}, connection)
-               container = ::Docker::Container.create(docker_hash, connection)
+               container = ::Docker::Container.create(docker_hash.dup, connection)
                #get full information of the container
                container = ::Docker::Container.get(container.id,connection)
             else
