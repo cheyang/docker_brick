92ddf0c67aa57a74bf533fbb5277810e900a3c77
cheyang <cheyang@163.com>
2015-01-14 15:42:01 +0800

    add options to new a service

--- lib/brick/cli/service_new.rb
+++ lib/brick/cli/service_new.rb
@@ -3,7 +3,22 @@ require 'brick/cli'
 class Brick::CLI::ServiceNew < Brick::CLI
   banner 'brick service new PROJECT(options)'
   
+   option :links, 
+    :long  => "--links service1,service2",
+    :default => nil,
+    :description => "Link to containers in another service"
+    
+    option :build, 
+    :long  => "--build centos",
+    :default => nil,
+    :description => "the service is built with Dockerfile from specified linux platform like redhat,centos,ubuntu"
+    
+    option :image, 
+    :long  => "--image redis",
+    :default => nil,
+    :description => "the service is from image"
+
   def run
-    puts banner
+    
   end
 end
